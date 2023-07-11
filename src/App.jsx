dc4c3ca20917ca2c785214cbe9ac13bd67021de5
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-07-11 13:04:46 +0530

    react app added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-black flex items-center justify-center h-screen w-screen">
+        Stay Tuned!
+      </p>
+    </>
+  );
+}
+
+export default App;
